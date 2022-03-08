d939341159cd4a478f242079e8e1994b77a3ab0f
Jil Patel <jilpatel0511@gmail.com>
2022-03-08 21:27:14 +0530

    day20 complete

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,8 +1,9 @@
 import 'package:first_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
-  const CartPage({ Key? key }) : super(key: key);
+  const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +12,61 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: Text('Cart'),
+      ),
+      body: Column(
+        children: [
+          _cartList().p32().expand(),
+          Divider(),
+          _cartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _cartTotal extends StatelessWidget {
+  const _cartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.xl4.color(MyTheme.darkBluishColor).make(),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Buy'),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.blueAccent),
+            ),
+          ).w32(context),
+        ],
+      ),
+    );
+  }
+}
+
+class _cartList extends StatefulWidget {
+  const _cartList({Key? key}) : super(key: key);
+
+  @override
+  State<_cartList> createState() => __cartListState();
+}
+
+class __cartListState extends State<_cartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.remove),
+        ),
+        title: Text('Item 1'),
       ),
     );
   }
